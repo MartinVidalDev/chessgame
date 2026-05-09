8f2e7a4e57defcdbc41a3bb3caebfa0e494f9bdd
Martin <martin.vidal@etudiant.univ-reims.fr>
2026-05-09 16:00:46 +0200

    feat: update board class to see pieces

--- Board/Board.cc
+++ Board/Board.cc
@@ -3,9 +3,33 @@
 Board::Board() {
     for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
-            board[i][j] = 0;
+            board[i][j] = Piece();
         }
     }
+
+    board[0][0] = Piece(PieceType::Rook, PieceColor::Black);
+    board[0][1] = Piece(PieceType::Knight, PieceColor::Black);
+    board[0][2] = Piece(PieceType::Bishop, PieceColor::Black);
+    board[0][3] = Piece(PieceType::Queen, PieceColor::Black);
+    board[0][4] = Piece(PieceType::King, PieceColor::Black);
+    board[0][5] = Piece(PieceType::Bishop, PieceColor::Black);
+    board[0][6] = Piece(PieceType::Knight, PieceColor::Black);
+    board[0][7] = Piece(PieceType::Rook, PieceColor::Black);
+
+    for (int i = 0; i < 8; i++)
+        board[1][i] = Piece(PieceType::Pawn, PieceColor::Black);
+
+    for (int i = 0; i < 8; i++)
+        board[6][i] = Piece(PieceType::Pawn, PieceColor::Black);
+
+    board[7][0] = Piece(PieceType::Rook, PieceColor::White);
+    board[7][1] = Piece(PieceType::Knight, PieceColor::White);
+    board[7][2] = Piece(PieceType::Bishop, PieceColor::White);
+    board[7][3] = Piece(PieceType::Queen, PieceColor::White);
+    board[7][4] = Piece(PieceType::King, PieceColor::White);
+    board[7][5] = Piece(PieceType::Bishop, PieceColor::White);
+    board[7][6] = Piece(PieceType::Knight, PieceColor::White);
+    board[7][7] = Piece(PieceType::Rook, PieceColor::White);
 }
 
 Board::~Board() {}
--- Board/Board.h
+++ Board/Board.h
@@ -1,11 +1,12 @@
 #ifndef __BOARD_H__
 #define __BOARD_H__
 #include <iostream>
+#include "../Piece/Piece.h"
 
 class Board
 {
 private:
-    int board[8][8];
+    Piece board[8][8];
 public:
     Board();
     ~Board();
