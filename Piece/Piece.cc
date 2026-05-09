1bd9c7b48b528567e5871cb3e14a4423cf454f1d
Martin <martin.vidal@etudiant.univ-reims.fr>
2026-05-09 16:00:18 +0200

    feat: create piece class

---
+++ Piece/Piece.cc
@@ -0,0 +1,46 @@
+#include "Piece.h"
+
+Piece::Piece() : type(PieceType::None), color(PieceColor::None) {}
+
+Piece::Piece(PieceType t, PieceColor c) : type(t), color(c) {}
+
+Piece::~Piece() {}
+
+PieceType Piece::getType() const {
+    return type;
+}
+PieceColor Piece::getColor() const {
+    return color;
+}
+
+void Piece::showPiece(std::ostream& o) const {
+    switch (type)
+    {
+    case PieceType::Pawn:
+        o << 'P';
+        break;
+    case PieceType::Rook:
+        o << 'R';
+        break;
+    case PieceType::Knight:
+        o << 'K';
+        break;
+    case PieceType::Bishop:
+        o << 'B';
+        break;
+    case PieceType::King:
+        o << 'K';
+        break;
+    case PieceType::Queen:
+        o << 'Q';
+        break;
+    
+    default:
+        o << ' ';
+    }
+}
+
+std::ostream& operator<<(std::ostream& os, const Piece& P) {
+    P.showPiece(os);
+    return os;
+}
---
+++ Piece/Piece.h
@@ -0,0 +1,39 @@
+#ifndef __PIECE_H__
+#define __PIECE_H__
+#include <iostream>
+
+enum class PieceColor {
+    White,
+    Black,
+    None
+};
+
+enum class PieceType {
+    Pawn,
+    Rook,
+    Knight,
+    Bishop,
+    King,
+    Queen,
+    None
+};
+
+class Piece
+{
+private:
+    PieceType type;
+    PieceColor color;
+public:
+    Piece();
+    Piece(PieceType t, PieceColor c);
+    ~Piece();
+    
+    PieceType getType() const;
+    PieceColor getColor() const;
+
+    void showPiece(std::ostream& o) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Piece& P);
+
+#endif
